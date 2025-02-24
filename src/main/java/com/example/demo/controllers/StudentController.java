a7d83d556c3c4871a3a88f2f537aeca58abf34cc
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-24 16:35:22 +0530

    Refactor StudentController to improve import statements and enhance code organization

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -4,7 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Student;
 import com.example.demo.entities.Teacher;
@@ -82,4 +89,5 @@ public class StudentController {
     public Optional<Teacher> getClassTeacher(@PathVariable int rollNo) {
         return service.getClassTeacher(rollNo);
     }
+    
 }
